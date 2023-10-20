a6a30a9330fc22c65e4307463f902fc899424226
Maysson <mayssonalexandre@gmai.com>
2023-10-20 19:02:52 -0400

    S

---
+++ aula18dia2909/c11ex3.py
@@ -0,0 +1,7 @@
+n1 = int(input())
+n2 = int(input())
+tot = 0
+
+for i in range(0,n1):
+    tot += n2
+print(tot)
---
+++ aula18dia2909/c11ex4.py
@@ -0,0 +1,5 @@
+for num in range(1,11):
+    for i in range(1,11):
+        atual = num * i
+        print(f'{num} * {i} = {atual}')
+    print()
